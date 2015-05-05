5018d7c4a5d4ef13f68ed79b5e3228043eabe10b
panticz <mail@konczalski.de>
2015-05-05 11:30:21 +0200

    add workaround for systemd

--- create.jessie.sh
+++ create.jessie.sh
@@ -4,6 +4,10 @@
 
 LANG=en_US.UTF-8
 sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r jessie
+
+# workaround for "Failed to mount cgroup at /sys/fs/cgroup/systemd: Permission denied"
+echo "lxc.aa_profile = unconfined" >> /var/lib/lxc/${CONTAINER}/config
+
 sudo lxc-start -d -n ${CONTAINER}
 
 sudo lxc-attach -n ${CONTAINER} -- apt-get clean
