2c14c18f3de91baa42a878a101e3d6d229adf12f
panticz <mail@konczalski.de>
2015-05-06 11:18:46 +0200

    remove unnecessary apt-get clean

--- create.jessie.sh
+++ create.jessie.sh
@@ -10,6 +10,5 @@ sudo echo "lxc.aa_profile = unconfined" | tee -a /var/lib/lxc/${CONTAINER}/confi
 
 sudo lxc-start -d -n ${CONTAINER}
 
-sudo lxc-attach -n ${CONTAINER} -- apt-get clean
 sudo lxc-attach -n ${CONTAINER} -- apt-get update
 sudo lxc-attach -n ${CONTAINER} -- apt-get dist-upgrade -y
