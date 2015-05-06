53c5545323fd12aab796f616f3500933255f4f80
panticz <mail@konczalski.de>
2015-05-06 11:20:40 +0200

    add comments

--- create.jessie.sh
+++ create.jessie.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 [ -z $CONTAINER ] && CONTAINER=jessie
-
 LANG=en_US.UTF-8
+
+# create container
 sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r jessie
 
 # workaround for "Failed to mount cgroup at /sys/fs/cgroup/systemd: Permission denied"
 sudo echo "lxc.aa_profile = unconfined" | tee -a /var/lib/lxc/${CONTAINER}/config
 
+# start container in background
 sudo lxc-start -d -n ${CONTAINER}
 
+# update packages in container
 sudo lxc-attach -n ${CONTAINER} -- apt-get update
 sudo lxc-attach -n ${CONTAINER} -- apt-get dist-upgrade -y
