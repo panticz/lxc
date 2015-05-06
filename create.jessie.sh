7af6e76419d3cbbfe20a937ee78ff520a89837a0
panticz <mail@konczalski.de>
2015-05-06 12:53:56 +0200

    add force option

--- create.jessie.sh
+++ create.jessie.sh
@@ -3,6 +3,11 @@
 [ -z ${CONTAINER} ] && CONTAINER=jessie
 LANG=en_US.UTF-8
 
+# force, remove previous container
+if [ "$1" == "-f" ]; then
+  [ $(sudo lxc-ls ${CONTAINER} | wc -l) -gt 0 ] && sudo lxc-destroy -f -n ${CONTAINER}
+fi
+
 # create container
 sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r jessie
 
