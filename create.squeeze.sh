edb68d52f1c1c1498f7954123b5911581f97414a
panticz <mail@konczalski.de>
2015-05-08 11:38:20 +0200

    backport from create.jessie.sh script
    https://github.com/panticz/lxc/blob/master/create.jessie.sh

--- create.squeeze.sh
+++ create.squeeze.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-[ -z $CONTAINER ] && CONTAINER=squeeze
-
+[ -z ${CONTAINER} ] && CONTAINER=squeeze
 LANG=en_US.UTF-8
+
+# force, remove previous container
+if [ "$1" == "-f" ]; then
+  [ $(sudo lxc-ls ${CONTAINER} | wc -l) -gt 0 ] && sudo lxc-destroy -f -n ${CONTAINER}
+fi
+
+# create container
 sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r squeeze
+
+# start container in background
 sudo lxc-start -d -n ${CONTAINER}
 
-sudo lxc-attach -n ${CONTAINER} -- apt-get clean
+# wait 3 seconds until network is up
+sleep 3
+
+# update packages in container
 sudo lxc-attach -n ${CONTAINER} -- apt-get update
 sudo lxc-attach -n ${CONTAINER} -- apt-get dist-upgrade -y
