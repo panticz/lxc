b7ecc337a2b873164e967f3a403daa9431a343ff
panticz <mail@konczalski.de>
2015-05-08 11:58:08 +0200

    backport from create.jessie.sh script

--- create.wheezy.sh
+++ create.wheezy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-[ -z $CONTAINER ] && CONTAINER=wheezy
-
+[ -z ${CONTAINER} ] && CONTAINER=wheezy
 LANG=en_US.UTF-8
+
+# force, remove previous container
+if [ "$1" == "-f" ]; then
+  [ $(sudo lxc-ls ${CONTAINER} | wc -l) -gt 0 ] && sudo lxc-destroy -f -n ${CONTAINER}
+fi
+
+# create container
 sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r wheezy
+
+# start container in background
 sudo lxc-start -d -n ${CONTAINER}
 
-sudo lxc-attach -n ${CONTAINER} -- apt-get clean
+# wait 3 seconds until network is up
+sleep 3
+
+# update packages in container
 sudo lxc-attach -n ${CONTAINER} -- apt-get update
 sudo lxc-attach -n ${CONTAINER} -- apt-get dist-upgrade -y
+
+# show containers status
+sudo lxc-ls ${CONTAINER} -f
