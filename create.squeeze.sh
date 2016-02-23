fe412a820a2f913a6e4724777af111669fe8b9ec
panticz <mail@konczalski.de>
2016-02-23 13:13:26 +0100

    Pass additional parameter to lxc-create

--- create.squeeze.sh
+++ create.squeeze.sh
@@ -6,10 +6,11 @@ LANG=en_US.UTF-8
 # force, remove previous container
 if [ "$1" == "-f" ]; then
   [ $(sudo lxc-ls ${CONTAINER} | wc -l) -gt 0 ] && sudo lxc-destroy -f -n ${CONTAINER}
+  shift
 fi
 
 # create container
-sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r squeeze
+sudo lxc-create -t debian -n ${CONTAINER} -- template-options -r squeeze $@
 
 # start container in background
 sudo lxc-start -d -n ${CONTAINER}
