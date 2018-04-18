5859dd6aebed260bd05cf84625cd641a6d5ebe7b
panticz <mail@konczalski.de>
2018-04-18 10:30:14 +0200

    specify full path to chroot

--- scripts/lxc-update-templates.sh
+++ scripts/lxc-update-templates.sh
@@ -12,8 +12,8 @@ LANG=en_US.UTF-8
 # update APT rootfs (Debian and Ubuntu)
 for DIST in $(find /var/cache/lxc/*/* -maxdepth 0 -type d); do
   echo "Updating ${DIST} ..."
-  chroot "${DIST}" apt-get update -qq
-  chroot "${DIST}" apt-get dist-upgrade -qq -y
-  chroot "${DIST}" apt-get autoremove -qq -y
-  chroot "${DIST}" apt-get clean
+  /usr/sbin/chroot "${DIST}" apt-get update -qq
+  /usr/sbin/chroot "${DIST}" apt-get dist-upgrade -qq -y
+  /usr/sbin/chroot "${DIST}" apt-get autoremove -qq -y
+  /usr/sbin/chroot "${DIST}" apt-get clean
 done
